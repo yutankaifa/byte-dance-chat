3484c5251f26155270d713a632b87bc2b0d7c2e1
yutankaifa <1989541473@qq.com>
2024-11-28 13:44:59 +0800

    chore: token

--- app/components/chat/ChatSetting.tsx
+++ app/components/chat/ChatSetting.tsx
@@ -12,6 +12,12 @@ import { useEffect, useState } from "react";
 import { SettingInter } from "~/types";
 import { getStorageSetting, setStorageSetting } from "~/utils/storage";
 import { toast } from "sonner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export default function ChatSetting() {
   useEffect(() => {
@@ -20,52 +26,55 @@ export default function ChatSetting() {
   }, []);
   const [setting, setSetting] = useState<SettingInter>();
   const saveSetting = () => {
-    console.log(setting);
     setStorageSetting(setting);
-    toast.success("设置成功");
+    window.location.reload();
   };
   return (
-    <div className="my-2 flex justify-center">
-      <div>
-        <Dialog onOpenChange={() => toast.dismiss()}>
-          <DialogTrigger>
-            <div className="flex gap-3 p-3 rounded-xl cursor-pointer hover:opacity-70">
-              <Cog6ToothIcon width={24} />
-              <span>设置</span>
-            </div>
-          </DialogTrigger>
-          <DialogContent className="min-h-[200px] max-w-screen-md rounded-2xl flex flex-col">
-            <DialogTitle className="hidden"></DialogTitle>
-            <div className="flex flex-col gap-3 flex-1">
-              <label htmlFor="token">Coze Secret Token</label>
-              <p className="text-sm text-red-500">
-                请输入正确的 token，否则将无法使用，获取 token 请访问
-                <a
-                  href="https://www.coze.cn/open/oauth/pats"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-blue-500"
-                >
-                  Coze 个人访问令牌
-                </a>
-              </p>
-              <Input
-                value={setting?.token}
-                onChange={(e) => setSetting({ token: e.target.value })}
-                width={300}
-                id="token"
-                placeholder="pat_bOI..."
-              />
-              <p className="text-sm text-gray-500">
-                注：该令牌只会保存到本地，不会上传到服务器
-              </p>
-            </div>
-            <DialogFooter>
-              <Button onClick={saveSetting}>保存</Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
+    <div className="flex items-center">
+      <Dialog onOpenChange={() => toast.dismiss()}>
+        <DialogTrigger style={{ height: "30px" }}>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger aria-label="设置">
+                <Cog6ToothIcon width={30} />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>设置</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DialogTrigger>
+        <DialogContent className="min-h-[200px] max-w-screen-md rounded-2xl flex flex-col">
+          <DialogTitle className="hidden"></DialogTitle>
+          <div className="flex flex-col gap-3 flex-1">
+            <label htmlFor="token">Coze Secret Token</label>
+            <p className="text-sm text-red-500">
+              请输入正确的 token，否则将无法使用，获取 token 请访问
+              <a
+                href="https://www.coze.cn/open/oauth/pats"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                Coze 个人访问令牌
+              </a>
+            </p>
+            <Input
+              value={setting?.token}
+              onChange={(e) => setSetting({ token: e.target.value })}
+              width={300}
+              id="token"
+              placeholder="pat_bOI..."
+            />
+            <p className="text-sm text-gray-500">
+              注：该令牌只会保存到本地，不会上传到服务器
+            </p>
+          </div>
+          <DialogFooter>
+            <Button onClick={saveSetting}>保存</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
